66ced7b3e65967a188f33320686b6ed8d0c16964
hackerman001 <godwinkvg@gmail.com>
2020-10-27 06:15:18 +0100

    Adding routes and Updating the veiw

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,35 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'home',
         loadChildren: () => import('../pages/tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
+        path: 'network',
         loadChildren: () => import('../pages/tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
+        path: 'post',
         loadChildren: () => import('../pages/tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
+      },
+      {
+        path: 'notifications',
+        loadChildren: () => import('../pages/tab4/tab4.module').then( m => m.Tab4PageModule)
+      },
+      {
+        path: 'jobs',
+        loadChildren: () => import('../pages/tab5/tab5.module').then( m => m.Tab5PageModule)
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full'
   }
 ];
