c24a7f0708a1ddbad72f1f7b1db793d4bb6af2a3
hackerman001 <godwinkvg@gmail.com>
2020-11-04 03:30:43 +0100

    Update  data to display differents posts

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -9,12 +9,12 @@ import { Component } from '@angular/core';
 export class Tab1Page {
   posts: Post[] = [
     {
-      authorName: 'Rachid Mayouna',
+      authorName: 'Postman',
       authorProfile: 'https://media-exp1.licdn.com/dms/image/C4E22AQFkEJp7eZXqYg/feedshare-shrink_800-alternative/0?e=1607558400&v=beta&t=Hbxlu43YJmRfXxRyLSi_K7tj2TB8DMLCQZ5VQLucUVo',
-      authorDescription: 'Entrepreneur',
+      authorDescription: 'REST API Documentation',
       content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, nisi, modi, architecto consectetur aliquid tempora earum aperiam officiis totam sunt tenetur! Incidunt unde dignissimos vero reiciendis consequuntur ipsa eius iste vitae. Voluptates corporis eum voluptatem aspernatur saepe magni, assumenda praesentium ex nesciunt. Distinctio, similique voluptatum.',
       time: Date.now() + 60000000,
-      contentImg: 'https://media-exp1.licdn.com/dms/image/C4E22AQFkEJp7eZXqYg/feedshare-shrink_800-alternative/0?e=1607558400&v=beta&t=Hbxlu43YJmRfXxRyLSi_K7tj2TB8DMLCQZ5VQLucUVo',
+      contentImg: 'https://media-exp1.licdn.com/dms/image/C5622AQEUT-_6ePU8sQ/feedshare-shrink_800-alternative/0?e=1607558400&v=beta&t=ibu9fJBE2NazenbZDTV-dw9KyAcziT_TbVwSsOb4VmI',
       likes: 10,
       comments: 35
     },
@@ -35,6 +35,26 @@ export class Tab1Page {
       content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, nisi, modi, architecto consectetur aliquid tempora earum aperiam officiis totam sunt tenetur! Incidunt unde dignissimos vero reiciendis consequuntur ipsa eius iste vitae. Voluptates corporis eum voluptatem aspernatur saepe magni, assumenda praesentium ex nesciunt. Distinctio, similique voluptatum.',
       time: Date.now() - 70002000,
       contentImg: 'https://media-exp1.licdn.com/dms/image/C4D22AQESOyvzzobsUA/feedshare-shrink_800-alternative/0?e=1607558400&v=beta&t=skfWOBxqVALXqSfuAGDgMfMOQFMOLOot2lQNVK5_C7Y',
+      likes: 23007,
+      comments: 30
+    },
+    {
+      authorName: 'Amanda Malela',
+      authorProfile: 'https://media-exp1.licdn.com/dms/image/C4D22AQESOyvzzobsUA/feedshare-shrink_800-alternative/0?e=1607558400&v=beta&t=skfWOBxqVALXqSfuAGDgMfMOQFMOLOot2lQNVK5_C7Y',
+      authorDescription: 'Voyageur | Yoga | Experimentateur',
+      content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, nisi, modi, architecto consectetur aliquid tempora earum aperiam officiis totam sunt tenetur! Incidunt unde dignissimos vero reiciendis consequuntur ipsa eius iste vitae. Voluptates corporis eum voluptatem aspernatur saepe magni, assumenda praesentium ex nesciunt. Distinctio, similique voluptatum.',
+      time: Date.now() - 70002000,
+      contentImg: 'https://media-exp1.licdn.com/dms/image/C5622AQGF5WRYFVWlqQ/feedshare-shrink_800-alternative/0?e=1607558400&v=beta&t=Idah9IY0c3Cdnl1aM5ZTRBgLxgVTJiUvECw9THXdlK4',
+      likes: 23007,
+      comments: 30
+    },
+    {
+      authorName: 'Rachid Mayouna',
+      authorProfile: 'https://media-exp1.licdn.com/dms/image/C4D22AQHC1o89uE2Cqg/feedshare-shrink_800-alternative/0?e=1607558400&v=beta&t=9BneD-B0pP7ztEuf_7tVTaNBoZqi_lSziouJuTCFehQ',
+      authorDescription: 'Entrepreneur ',
+      content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, nisi, modi, architecto consectetur aliquid tempora earum aperiam officiis totam sunt tenetur! Incidunt unde dignissimos vero reiciendis consequuntur ipsa eius iste vitae. Voluptates corporis eum voluptatem aspernatur saepe magni, assumenda praesentium ex nesciunt. Distinctio, similique voluptatum.',
+      time: Date.now() - 70002000,
+      contentImg: 'https://media-exp1.licdn.com/dms/image/C4D22AQHC1o89uE2Cqg/feedshare-shrink_800-alternative/0?e=1607558400&v=beta&t=9BneD-B0pP7ztEuf_7tVTaNBoZqi_lSziouJuTCFehQ',
       likes: 23007,
       comments: 30
     },
