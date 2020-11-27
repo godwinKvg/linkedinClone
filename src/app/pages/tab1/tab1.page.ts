618f33817649e5b50f1d4d7d4fc19b0718e28971
hackerman001 <godwinkvg@gmail.com>
2020-11-27 10:04:36 +0100

    Add infinite scroll to post

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,13 +1,15 @@
 import { Router } from '@angular/router';
+import { IonInfiniteScroll } from '@ionic/angular';
 import { PostService } from './../../services/post/post.service';
 import { IPost } from './../../models/post.models';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
   posts: IPost[];
 
@@ -16,6 +18,22 @@ export class Tab1Page {
     private postService: PostService,
   ) {
     this.posts = postService.posts;
+  }
+
+
+
+  loadData(event) {
+    setTimeout(() => {
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.posts.length == 1000) {
+        event.target.disabled = true;
+      }
+
+      this.posts = [...this.posts, ...this.posts]
+    }, 500);
   }
 
   onGoPost() {
